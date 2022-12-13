4509544c7c4200ce60224285aa70abeeea5935a9
RONNEL NAVARRO <uptimisedcorporation2022@gmail.com>
2022-12-14 05:17:07 +0800

    transfer file 7

---
+++ system/admin/index.php
@@ -0,0 +1,58 @@
+<?php require_once 'include/header.php' ?>
+<?php require_once 'include/navbar.php' ?>
+<?php require_once 'include/topbar.php' ?>
+<div class="pcoded-wrapper">
+  <div class="pcoded-content">
+    <div class="pcoded-inner-content">
+        <!-- Main-body start -->
+        <div class="main-body">
+          <div class="page-wrapper">
+            <!-- Page header start -->
+            <div class="page-header m-t-10">
+              <div class="page-header-title">
+                <h4>Horizontal Fixed Layout</h4>
+              </div>
+              <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                  <li class="breadcrumb-item">
+                    <a href="index.html">
+                      <i class="icofont icofont-home"></i>
+                    </a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="#!">Page Layouts</a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="#!">Horizontal</a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="#!">Fixed Layout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- Page header end -->
+
+            <!-- Page body start -->
+            <div class="page-body">
+              <div class="row">
+                <div class="col-lg-12">
+                  <!-- Default card start -->
+                  <div class="card">
+                    <div class="card-block">
+                      <span>
+                        Horizontal Fixed layout is useful for those users who wants header menu options on top.
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <!-- Default card end -->
+              </div>
+            </div>
+          </div>
+          <!-- Page body end -->
+        </div>
+      </div>
+      <!-- Main-body end -->
+      <div id="styleSelector">
+      </div>
+  </div>
+</div>
+<?php require_once 'include/footer.php' ?>
